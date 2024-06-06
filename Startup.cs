b93abf0f0d42b64fd64fb7d5869d36d5a0bd614f
Milos Milosavljevic <mmilosavljevic@comtrade.com>
2024-06-06 11:58:04 +0200

    test

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace Botina
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.TEST
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHttpClient().AddControllers().AddNewtonsoftJson(options =>
